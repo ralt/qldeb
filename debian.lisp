fd42ad83b3165a4f40f952928ef6aacd91e7b4fe
Florian MARGAINE <florian@margaine.com>
2015-12-06 23:14:10 +0100

    Add some comments

--- debian.lisp
+++ debian.lisp
@@ -5,12 +5,15 @@
   (build-package env system)
   (copy-package env system))
 
-(defun debianize-system (env system))
+(defun debianize-system (env system)
+  "Debianizing a system is just creating the debian/ folder and
+put some generated files in there.")
 
 (defun build-package (env system)
   (uiop:chdir (merge-pathnames
                (uiop:strcat (ql-dist:prefix system) "/")
                (merge-pathnames #p"root/common-lisp" env)))
+  ;; Don't sign the packages for now.
   (uiop:run-program "dpkg-buildpackage -us -uc"))
 
 (defun copy-package (env system)
