1810357b3fe6acb39b35ed1a49a6bc8540c72516
Florian MARGAINE <florian@margaine.com>
2015-12-06 23:22:08 +0100

    Start implementation of debianzing

--- debian.lisp
+++ debian.lisp
@@ -7,12 +7,35 @@
 
 (defun debianize-system (env system)
   "Debianizing a system is just creating the debian/ folder and
-put some generated files in there.")
+put some generated files in there."
+  (let* ((system-folder (merge-pathnames
+                         (uiop:strcat (ql-dist:prefix system) "/")
+                         (merge-pathnames #p"root/common-lisp/" env)))
+         (debian-folder (merge-pathnames #p"debian/" system-folder)))
+    (loop for file in `((,#'control-file . "control")
+                        (,#'changelog-file . "changelog")
+                        (,#'compat-file . "compat")
+                        (,#'copyright-file . "copyright")
+                        (,#'rules-file . "rules"))
+       do (with-open-file (f (merge-pathnames (cdr file) debian-folder)
+                             :direction :output
+                             :if-does-not-exist :create)
+            (write-sequence (funcall (car file) system) f)))))
+
+(defun control-file (system))
+
+(defun changelog-file (system))
+
+(defun compat-file (system))
+
+(defun copyright-file (system))
+
+(defun rules-file (system))
 
 (defun build-package (env system)
   (uiop:chdir (merge-pathnames
                (uiop:strcat (ql-dist:prefix system) "/")
-               (merge-pathnames #p"root/common-lisp" env)))
+               (merge-pathnames #p"root/common-lisp/" env)))
   ;; Don't sign the packages for now.
   (uiop:run-program "dpkg-buildpackage -us -uc"))
 
