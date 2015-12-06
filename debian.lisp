4481ad9409321218082a5a870fc6d3aeb3c80d56
Florian MARGAINE <florian@margaine.com>
2015-12-06 22:43:06 +0100

    Foundations

--- debian.lisp
+++ debian.lisp
@@ -1,3 +1,12 @@
 (in-package #:qldeb)
 
-(defun build-debian-package (env system))
+(defun build-debian-package (env system)
+  (debianize-system env system)
+  (build-package env system)
+  (copy-package env system))
+
+(defun debianize-system (env system))
+
+(defun build-package (env system))
+
+(defun copy-package (env system))
