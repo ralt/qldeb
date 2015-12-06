84782ea3b9afd4b33daeae6abd8e560e048df4cc
Florian MARGAINE <florian@margaine.com>
2015-12-06 23:08:39 +0100

    Fix copy-package and add build-package

--- debian.lisp
+++ debian.lisp
@@ -7,13 +7,33 @@
 
 (defun debianize-system (env system))
 
-(defun build-package (env system))
+(defun build-package (env system)
+  (uiop:chdir (merge-pathnames
+               (uiop:strcat (ql-dist:prefix system) "/")
+               (merge-pathnames #p"root/common-lisp" env)))
+  (uiop:run-program "dpkg-buildpackage -us -uc"))
 
 (defun copy-package (env system)
-  (let ((package (package-filename system)))
-    (rename-file (merge-pathnames package env) package)))
+  (dolist (file (package-files system))
+    (rename-file (merge-pathnames file
+                                  (merge-pathnames #p"root/common-lisp/" env))
+                 file)))
 
-(defun package-filename (system)
-  (uiop:strcat (ql-dist:project-name system)
-               "-" (ql-dist:version (ql-dist:dist system))
-               "_amd64.deb"))
+(defun package-files (system)
+  (list
+   ;; .deb
+   (uiop:strcat (ql-dist:project-name system)
+                "_" (ql-dist:version (ql-dist:dist system))
+                "_amd64.deb")
+   ;;; .changes
+   (uiop:strcat (ql-dist:project-name system)
+                "_" (ql-dist:version (ql-dist:dist system))
+                "_amd64.changes")
+   ;;; .dsc
+   (uiop:strcat (ql-dist:project-name system)
+                "_" (ql-dist:version (ql-dist:dist system))
+                ".dsc")
+   ;;; .tar.xz
+   (uiop:strcat (ql-dist:project-name system)
+                "_" (ql-dist:version (ql-dist:dist system))
+                ".tar.xz")))
