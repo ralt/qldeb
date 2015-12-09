f2c816e4103a2bd2e25acb3f6e1f8b3ebe89dc15
Florian MARGAINE <florian@margaine.com>
2015-12-09 23:13:45 +0100

    Open the file after checking the release is ok

--- release.lisp
+++ release.lisp
@@ -24,21 +24,20 @@
            (merge-pathnames name #p"/root/common-lisp/"))))
 
 (defun download-release (env release path url)
-  (with-open-file (f path
-                     :direction :output
-                     :element-type '(unsigned-byte 8)
-                     :if-does-not-exist :create)
-    (format t "downloading ~A...~%" url)
-    (let ((archive (drakma:http-request url :force-binary t)))
-      (unless (check-archive release archive)
-        (format t "~A corrupted, trying again...~%" (namestring path))
-        (return-from download-release (download-release env release path url)))
-      (write-sequence archive f)
-      (format t "~A is downloaded to ~A~%" url (namestring path)))))
+  (format t "downloading ~A...~%" url)
+  (let ((archive (drakma:http-request url :force-binary t)))
+    (unless (check-archive release archive)
+      (format t "~A corrupted, trying again...~%" (namestring path))
+      (return-from download-release (download-release env release path url)))
+    (with-open-file (f path
+                       :direction :output
+                       :element-type '(unsigned-byte 8)
+                       :if-does-not-exist :create)
+      (write-sequence archive f))
+    (format t "~A is downloaded to ~A~%" url (namestring path))))
 
 (defun check-archive (release archive)
   (and (check-archive-sha1 (ql-dist:archive-content-sha1 release) archive)
-       (check-archive-md5 (ql-dist::archive-md5 release) archive)
        (check-archive-size (ql-dist:archive-size release) archive)))
 
 (defun sum (digest value)
@@ -47,9 +46,6 @@
 
 (defun check-archive-sha1 (sum archive)
   (string= sum (sum :sha1 archive)))
-
-(defun check-archive-md5 (sum archive)
-  (string= sum (sum :md5 archive)))
 
 (defun check-archive-size (size archive)
   (= (length archive) size))
