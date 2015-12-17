e6ef54ed5cd980e63a102abbfd0b46d0f440e330
Florian MARGAINE <florian@margaine.com>
2015-12-17 23:45:51 +0100

    Remove the hardcoded values

--- qldeb.lisp
+++ qldeb.lisp
@@ -13,11 +13,12 @@
   (with-temp (env)
     (let ((releases-channel (lparallel:make-channel))
           (builds-channel (lparallel:make-channel))
-          (dist (ql-dist:find-dist "quicklisp")))
-      (dolist (release (subseq (ql-dist:provided-releases dist) 0 10))
+          (dist (ql-dist:find-dist "quicklisp"))
+          (releases (ql-dist:provided-releases dist)))
+      (dolist (release releases)
         (lparallel:submit-task releases-channel #'install-release
                                env release (find-systems dist release)))
-      (dotimes (i 10)
+      (dotimes (i (1- (length releases)))
         (declare (ignore i))
         (lparallel:submit-task builds-channel
                                (lambda (systems)
@@ -25,7 +26,7 @@
                                    (build-debian-package env system)))
                                (lparallel:receive-result releases-channel)))
       ;; Just wait for the builds to be done
-      (dotimes (i 10)
+      (dotimes (i (1- (length releases)))
         (declare (ignore i))
         (lparallel:receive-result builds-channel)))))
 
